4a982ff1733af2217e3ddcee84a008ec710c12e9
Chandler Lofland <chandlerl14@outlook.com>
2020-02-17 09:38:57 -0500

    fixing action

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo Starting Yocto build process.
-source build/sources/poky/oe-init-build-env
-bitbake rpi-basic-image
--- install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping
