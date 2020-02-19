4513d74b9f34740835510cf88b456567dcb3070d
Chandler Lofland <chandlerl14@outlook.com>
2020-02-19 09:24:26 -0500

    more updates to install scripts

--- mk1/install.sh
+++ mk1/install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+git submodule init
+git submodule update --remote
 sudo apt-get install zip libglib2.0-dev gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm -y
