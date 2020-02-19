3fe2a34d1c0c7cb3aabbb4b8873c625c6f457498
Chandler Lofland <chandlerl14@outlook.com>
2020-02-19 09:23:21 -0500

    update config and add package for build

--- mk1/install.sh
+++ mk1/install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install zip gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm -y
+sudo apt-get install zip libglib2.0-dev gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm -y
