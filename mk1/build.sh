dda6b9d096651054b14ed602b0f99d4708b471d6
Chandler Lofland <chandlerl14@outlook.com>
2020-02-19 09:17:47 -0500

    updated build and install scripts

--- mk1/build.sh
+++ mk1/build.sh
@@ -2,3 +2,7 @@
 echo Starting build process.....
 source build/sources/poky/oe-init-build-env
 bitbake iot-image
+echo Build Complete. Archiving ISO.....
+mv tmp/deploy/images/raspberrypi0-wifi/iot-image.raspberrypi0-wifi.sd-img ./iot-image.raspberrypi0-wifi.img
+zip iot-image.zip *.img
+echo Build Complete.
